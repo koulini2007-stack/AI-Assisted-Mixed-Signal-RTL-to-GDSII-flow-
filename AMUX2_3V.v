cfd67f4061255edf4f204f1b1ada809dc3b9ca75
koulini2007-stack <koulini2007@gmail.com>
2026-07-13 18:53:15 +0530

    Create AMUX2_3V.v

---
+++ AMUX2_3V.v
@@ -0,0 +1,14 @@
+// AI generated analog mux blackbox
+// SKY130 mixed signal OpenLane flow
+
+(* blackbox *)
+module AMUX2_3V (
+    input I0,
+    input I1,
+    input select,
+    output out,
+    inout VPWR,
+    inout VGND
+);
+
+endmodule
